298d707d4141b815108f88356962986f2612a7ca
cenTimo13 <timobernardo13@gmail.com>
2024-10-08 23:23:50 +0800

    Initial Commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+    public function about(): string
+    {
+        return view('aboutUs');
+    }
+    public function products(): string
+    {
+        return view('products');
+    }
+}
